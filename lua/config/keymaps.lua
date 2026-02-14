060f662ed3c28f0a3de284ba8fbb03566fba0ecf
Cryplo <dylanliaa@gmail.com>
2026-02-14 12:14:01 -0500

    only keep settings that differ from lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,42 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Only keymaps that DIFFER from or ADD to LazyVim defaults are listed here.
 
 local map = vim.keymap.set
 
--- Fast saving
+-- Fast saving (LazyVim doesn't have this)
 map("n", "<leader>w", ":w!<CR>", { desc = "Save file" })
 
--- Smart way to move between windows (LazyVim uses these by default, but ensure they work)
-map("n", "<C-j>", "<C-W>j", { desc = "Move to window below" })
-map("n", "<C-k>", "<C-W>k", { desc = "Move to window above" })
-map("n", "<C-h>", "<C-W>h", { desc = "Move to window left" })
-map("n", "<C-l>", "<C-W>l", { desc = "Move to window right" })
-
--- Buffer navigation
+-- Buffer navigation (overrides LazyVim's <leader>l which opens Lazy manager)
 map("n", "<leader>l", ":bnext<CR>", { desc = "Next buffer" })
 map("n", "<leader>h", ":bprevious<CR>", { desc = "Previous buffer" })
-map("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
-map("n", "<leader>ba", ":bufdo bd<CR>", { desc = "Delete all buffers" })
 
--- Tab management
-map("n", "<leader>tn", ":tabnew<CR>", { desc = "New tab" })
-map("n", "<leader>to", ":tabonly<CR>", { desc = "Close other tabs" })
-map("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab" })
-map("n", "<leader>t<leader>", ":tabnext<CR>", { desc = "Next tab" })
-
--- Disable highlight when <leader><cr> is pressed
+-- Clear search highlight (LazyVim uses <leader>ur)
 map("n", "<leader><CR>", ":noh<CR>", { silent = true, desc = "Clear search highlight" })
 
 -- Remap 0 to first non-blank character
 map("n", "0", "^", { desc = "Go to first non-blank" })
 
--- Move lines with Alt+j/k
-map("n", "<M-j>", ":m .+1<CR>==", { desc = "Move line down" })
-map("n", "<M-k>", ":m .-2<CR>==", { desc = "Move line up" })
-map("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-map("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+-- Space to search (LazyVim uses Space as leader)
+map("n", "<Space>", "/", { desc = "Search" })
 
--- Spell checking
+-- Spell checking shortcuts
 map("n", "<leader>ss", ":setlocal spell!<CR>", { desc = "Toggle spell check" })
 map("n", "<leader>sn", "]s", { desc = "Next misspelled word" })
 map("n", "<leader>sp", "[s", { desc = "Previous misspelled word" })
@@ -46,27 +30,21 @@ map("n", "<leader>s?", "z=", { desc = "Spelling suggestions" })
 -- Toggle paste mode
 map("n", "<leader>pp", ":setlocal paste!<CR>", { desc = "Toggle paste mode" })
 
--- Quick open buffer for scribble
+-- Quick scratch buffers
 map("n", "<leader>q", ":e ~/buffer<CR>", { desc = "Open scratch buffer" })
 map("n", "<leader>x", ":e ~/buffer.md<CR>", { desc = "Open markdown scratch" })
 
--- Terminal escape (important for terminal mode)
+-- Terminal escape
 map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Visual mode: search for selection with * or #
+-- Visual mode: search for selection
 map("v", "*", [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], { desc = "Search selection forward" })
 map("v", "#", [[y?\V<C-R>=escape(@",'/\')<CR><CR>]], { desc = "Search selection backward" })
-
--- Space to search (like your original config)
-map("n", "<Space>", "/", { desc = "Search" })
 
 -- Quick edit config
 map("n", "<leader>e", ":e ~/.config/nvim/lua/config/options.lua<CR>", { desc = "Edit config" })
 
--- Git gutter toggle (if using gitsigns)
-map("n", "<leader>d", ":Gitsigns toggle_signs<CR>", { desc = "Toggle git signs" })
-
--- Quickfix navigation
+-- Quickfix navigation (your shortcuts)
 map("n", "<leader>cc", ":botright copen<CR>", { desc = "Open quickfix" })
 map("n", "<leader>n", ":cn<CR>", { desc = "Next quickfix item" })
 map("n", "<leader>p", ":cp<CR>", { desc = "Previous quickfix item" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,69 +1,31 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Only settings that DIFFER from LazyVim defaults are listed here.
 
--- Leader key (set before lazy.nvim loads)
+-- Leader key (LazyVim uses Space, you use comma)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
--- General
-vim.opt.history = 500
-vim.opt.autoread = true
+-- Tabs/indentation (LazyVim uses 2, you use 4)
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
--- UI
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 7 -- Keep 7 lines visible when scrolling
-vim.opt.wildmenu = true
-vim.opt.wildignore = { "*.o", "*~", "*.pyc", "*/.git/*", "*/.hg/*", "*/.svn/*", "*/.DS_Store" }
-vim.opt.ruler = true
-vim.opt.cmdheight = 1
-vim.opt.hidden = true
-vim.opt.backspace = { "eol", "start", "indent" }
-vim.opt.whichwrap:append("<,>,h,l")
-vim.opt.lazyredraw = true
+-- UI differences
+vim.opt.scrolloff = 7       -- LazyVim: 4
+vim.opt.laststatus = 2      -- LazyVim: 3 (global statusline)
+vim.opt.wrap = true         -- LazyVim: false
+vim.opt.ruler = true        -- LazyVim: false
+vim.opt.cmdheight = 1       -- LazyVim: 0
+vim.opt.foldcolumn = "1"    -- LazyVim: "0"
+
+-- Your additions (not in LazyVim)
+vim.opt.history = 500
+vim.opt.wildignore:append({ "*/.git/*", "*/.hg/*", "*/.svn/*", "*/.DS_Store" })
 vim.opt.magic = true
 vim.opt.showmatch = true
 vim.opt.matchtime = 2
-vim.opt.foldcolumn = "1"
-
--- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-
--- No annoying sounds
-vim.opt.errorbells = false
-vim.opt.visualbell = false
-
--- Colors
-vim.opt.background = "dark"
-vim.opt.termguicolors = true
-
--- Encoding
-vim.opt.encoding = "utf-8"
-vim.opt.fileformats = { "unix", "dos", "mac" }
-
--- Files, backups, undo
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.swapfile = false
-vim.opt.undofile = true
-vim.opt.undodir = vim.fn.expand("~/.config/nvim/undo")
-
--- Tabs and indentation
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = true
-vim.opt.linebreak = true
 vim.opt.textwidth = 500
-
--- Status line (LazyVim uses lualine, but ensure it's always visible)
-vim.opt.laststatus = 2
+vim.opt.shortmess:append("A")  -- Stop 'Press ENTER' prompts
 
 -- Disable auto-commenting on new lines
 vim.api.nvim_create_autocmd("FileType", {
@@ -73,17 +35,7 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- Return to last edit position when opening files
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = function()
-    local line = vim.fn.line("'\"")
-    if line > 1 and line <= vim.fn.line("$") then
-      vim.cmd('normal! g\'"')
-    end
-  end,
-})
-
--- Delete trailing whitespace on save for certain filetypes
+-- Delete trailing whitespace on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = { "*.txt", "*.js", "*.py", "*.sh", "*.lua" },
   callback = function()
@@ -97,9 +49,5 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_user_command("W", "write", {})
 vim.api.nvim_create_user_command("Wq", "wqa", {})
 vim.api.nvim_create_user_command("WQ", "wqa", {})
-vim.api.nvim_create_user_command("Wa", "wa", {})
 vim.api.nvim_create_user_command("Q", "quit", {})
 vim.api.nvim_create_user_command("Qa", "qa", {})
-
--- Stop the 'Press ENTER' prompt
-vim.opt.shortmess:append("A")
